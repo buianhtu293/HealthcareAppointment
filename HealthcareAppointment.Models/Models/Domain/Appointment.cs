c587b50b6d661a7dadda6d0abfc79da2cd3c3674
tubahe171912 <tubahe171912@fpt.edu.vn>
2024-10-11 13:43:57 +0700

    Add project files.

---
+++ HealthcareAppointment.Models/Models/Domain/Appointment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthcareAppointment.Models.Models.Domain
+{
+    public class Appointment
+    {
+        public Guid Id { get; set; }
+        public Guid PatientId { get; set; }
+        public User PatientUser { get; set; } = null!;
+        public Guid DoctorId { get; set; }
+        public User DoctorUser { get; set; } = null!;
+        public DateOnly Date { get; set; }
+        public int Status { get; set; }
+    }
+}
---
+++ HealthcareAppointment.Models/Models/Domain/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthcareAppointment.Models.Models.Domain
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public string Password { get; set; }
+        public int Role { get; set; }
+        public string Specialization { get; set; }
+
+        public ICollection<Appointment> AppointmentPatients { get; } = new List<Appointment>();
+        public ICollection<Appointment> AppointmentDoctors { get; } = new List<Appointment>();
+    }
+}
